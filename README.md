9bfe69408447eb1ea2ada9577504ea23906195f2
Luke Rohenaz <rohenaz@gmail.com>
2023-12-23 13:19:15 -0500

    init

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# hx-filter
+# hx-transform
 
 To install via CDN:
 
 ```html
-<script src="https://cdn.jsdelivr.net/npm/hx-filter/index.js" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/hx-transform/index.js" defer></script>
 ```
 
 or via npm if you prefer:
@@ -16,5 +16,31 @@ Then add the following to your HTML:
 
 ```html
 <script src="/path/to/htmx.js"></script>
-<script src="/node_modules/hx-filter/index.js"></script>
+<script src="/node_modules/hx-transform/index.js"></script>
+```
+
+# Example Usage
+
+Lets say we have an endpoint that returns JSON like this:
+
+```json
+{
+  "success": true,
+  "message": "Profile saved successfully"
+}
+```
+
+```html
+<div id="save-profile-status" hx-ext="remove-me" class="flex mb-2"></div>
+
+<button
+  type="submit"
+  class="btn btn-primary bg-primary text-primary-content"
+  hx-post="/profile"
+  hx-target="#save-profile-status"
+  hx-ext="transform-response"
+  hx-transform="(response) => response.success ? <div remove-me='3s'>success</div> : error"
+>
+  Save
+</button>
 ```
--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-window.htmx.defineExtension("filter-response", {
-  handleSwap: function (swapStyle, target, fragment, settleInfo) {
-    return false;
-  },
+window.htmx.defineExtension("transform-response", {
   transformResponse: function (text, xhr, request) {
-    // get the hx-filter attribute from the request element
-    const filter = request.getAttribute("hx-filter");
+    // get the hx-transform attribute from the request element
+    const transform = request.getAttribute("hx-transform");
+    if (transform) {
+      const transformJs = transform.trim();
+      let parsed;
 
-    if (filter) {
-      // check if the filter contains a space
-      if (filter.indexOf(":") > -1 && filter.indexOf("?") > -1) {
-        // if it does, split it into an array at the punctuation
-        const filters = filter.split(/[:?]/);
-
-        // and return the value of the property named by the first item in the array
-        const parsed = JSON.parse(text);
-        const filterValue = parsed[filters[0].trim()];
-        return filterValue === "true" || filterValue === true
-          ? filters[1]
-          : filters[2];
+      // if the response type is JSON, parse it
+      if (
+        xhr.getResponseHeader("Content-Type").startsWith("application/json")
+      ) {
+        try {
+          // try to parse as JSON
+          parsed = JSON.parse(text);
+        } catch (e) {
+          // if not JSON, just return the text
+          return text;
+        }
+      } else if (
+        xhr.getResponseHeader("Content-Type").startsWith("text/html")
+      ) {
+        return text;
+      } else if (
+        xhr.getResponseHeader("Content-Type").startsWith("text/plain")
+      ) {
+        return text;
       }
-      throw new Error(
-        "Invalid filter. Expecxting a turnary statement. ex: 'valid ? success : error'"
-      );
+
+      // execute inline function
+      if (transformJs.startsWith("function") || transformJs.startsWith("(")) {
+        const transformFunction = eval(transformJs);
+        return transformFunction(parsed);
+      }
+
+      // execute function by name
+      const transformFunction = eval(transformJs);
+      return transformFunction(parsed);
     }
+
     return text;
   },
 });
